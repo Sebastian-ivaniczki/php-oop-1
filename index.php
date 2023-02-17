3e43133f946b4819b97b242d99808f7345c51f46
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-02-17 14:37:27 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+
+*/
+
+class Movie
+{
+    public $title;
+    public $genre;
+    public $vote;
+    public $plot;
+
+    public function __construct($title, $genre, $vote = 0, $plot)
+    {
+        $this->title = $title;
+        $this->genre = $genre;
+        $this->vote = $vote;
+        $this->plot = $plot;
+    }
+
+    public function getTitle(){
+        return $this->title;
+    }
+};
