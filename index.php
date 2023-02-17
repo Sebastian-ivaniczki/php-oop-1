6f2469986d8e70c2c6fcd9b03f63a6a875fb8dff
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-02-17 15:22:02 +0100

    print film on page

--- index.php
+++ index.php
@@ -47,9 +47,41 @@ $movies = [$movie_1, $movie_2];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container mt-5">
+         <?php foreach ($movies as $movie) { ?>
+            <div class="card">
+                <div>
+                    <h1>
+                        
+                        <?php echo $movie->getTitle() ?>
+                    </h1>
+                </div>
+                <div>
+                    <h2>
+                        Genre:
+                        <?php echo $movie->getGenre() ?>
+                    </h2>
+                </div>
+                <div>
+                    <h3>
+                        Vote:
+                        <?php echo $movie->getVote() ?>
+                    </h3>
+                </div>
+                <div>
+                    <h4>
+                        Plot:
+                        <?php echo $movie->getPlot() ?>
+                    </h4>
+                </div>
+            </div>
+        <?php } ?>
+        
+    </div>
 </body>
 </html>
